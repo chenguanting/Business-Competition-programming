4f1771cc6d4e636fc5905a79ec5592a4b0fc3ecf
chenguanting <35651164+chenguanting@users.noreply.github.com>
2020-09-28 23:16:46 +0800

    Update problem1.java
    增加註解

--- problem1.java
+++ problem1.java
@@ -4,10 +4,10 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.util.TreeSet;
 import java.io.PrintWriter;
-
+//****找出這字串中阿拉伯數的字元****
 public class problem1 {
 
-    public static void A(String filename,PrintWriter output) throws FileNotFoundException {
+    public static void A(String filename,PrintWriter output) throws FileNotFoundException {//核心邏輯 建立方法
         File file = new File(filename);
         Scanner input = new Scanner(file);
         TreeSet<String> hset = new TreeSet<>();
@@ -17,11 +17,11 @@ public class problem1 {
             String str1 = input.nextLine();
             for (int i = 0; i < str1.length(); i++) {
 
-                if (str1.charAt(i) >= 48 && str1.charAt(i) <= 57) {
+                if (str1.charAt(i) >= 48 && str1.charAt(i) <= 57) {//轉換ASCII
                     char str2 = str1.charAt(i);
                     String str3 = String.valueOf(str2);
 
-                    hset.add(str3);
+                    hset.add(str3);//存到TREESET 不重複且排序
 
                 } else if ((i == (str1.length()-1)) && ((str1.charAt(i) >= 48 && str1.charAt(i) <= 57)==false)) {
                     output.print("N");
@@ -29,7 +29,7 @@ public class problem1 {
 
             }
 
-            Iterator k = hset.iterator();
+            Iterator k = hset.iterator();//迭代輸出
             while (k.hasNext()) {
 
                 output.print(k.next());
@@ -51,7 +51,7 @@ public class problem1 {
         A( "C:\\Users\\Guan Ting\\Downloads\\術科檢測資料_1\\術科檢測資料_1\\Problem1\\子題1\\in1.txt",output);
         output.println();
         A( "C:\\Users\\Guan Ting\\Downloads\\術科檢測資料_1\\術科檢測資料_1\\Problem1\\子題1\\in2.txt" ,output);
-        output.close();
+        output.close();//記得關閉
 
     }
 }
